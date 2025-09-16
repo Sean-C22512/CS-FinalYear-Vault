f2ece6d48b9292294f5bd39d9ad25de8a61b20fc
Sean-C22512 <124142280+Sean-C22512@users.noreply.github.com>
2025-09-16 13:54:21 +0100

    vault backup: 2025-09-16 13:54:21

--- CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_Pandas.py
+++ CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_Pandas.py
@@ -1,19 +1,18 @@
+# Q4
 import pandas as pd
 loans = pd.read_csv('loans.csv', header=None)  # Use relative path since the file is in the same directory
 
-print(loans[2])
-print("\n\n\n")
-print(loans[2][5])
+# #5
+# print(loans[2])
+# print("\n\n\n")
+# print(loans[2][5])
+
+# #6
 loans.columns = ['id', 'occupation', 'age', 'loan-to-salary', 'outcome']
-print(f"loans['age'] - >\n{loans['age']}")
-print("\n\n\n")
-print(f"loans.age - >\n{loans.age}")
+# print(f"loans['age'] - >\n{loans['age']}")
+# print("\n\n\n")
+# print(f"loans.age - >\n{loans.age}")
 
+#7
 
-
-
-
-
-
-
-
+print(loans.dtypes)
