fb630a8354a93dd882c28fe0f0f246716707d6f8
Sean-C22512 <124142280+Sean-C22512@users.noreply.github.com>
2025-09-19 15:33:22 +0100

    vault backup: 2025-09-19 15:33:22

--- CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_PD_Wines.py
+++ CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_PD_Wines.py
@@ -9,5 +9,9 @@ first_row = reviews.iloc[0]
 first_descriptions = reviews.description.iloc[0:10]
 sample_reviews = reviews.iloc[[1,2,3,5,8]]
 
+rows = [0,1,10,100]
+cols = ['country', 'province', 'region_1', 'region_2']
+df = reviews.loc[rows][cols]
+
 
 
