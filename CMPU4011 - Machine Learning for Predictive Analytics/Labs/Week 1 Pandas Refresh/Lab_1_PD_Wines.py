e18d9f4a42f1727214c27d4e7af919e95c84cd14
Sean-C22512 <124142280+Sean-C22512@users.noreply.github.com>
2025-09-19 15:13:06 +0100

    vault backup: 2025-09-19 15:13:06

--- CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_PD_Wines.py
+++ CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_PD_Wines.py
@@ -6,8 +6,7 @@ reviews = pd.read_csv('winemag-data-130k-v2.csv')
 desc = reviews['description']
 #first_description = reviews.description.iloc[1]
 first_row = reviews.iloc[0]
-first_descriptions = reviews.iloc[0]
+first_descriptions = reviews.description.iloc[0:10]
 
-# for i in range(10):
-#     print(f"\n\n{reviews.iloc[i]}")
+
 
