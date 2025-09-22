11b76bb1649f5fe08f7911e5f67a675a21c258ee
Sean-C22512 <124142280+Sean-C22512@users.noreply.github.com>
2025-09-22 10:29:14 +0100

    vault backup: 2025-09-22 10:29:14

--- CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_PD_Wines.py
+++ CMPU4011 - Machine Learning for Predictive Analytics/Labs/Week 1 Pandas Refresh/Lab_1_PD_Wines.py
@@ -10,15 +10,22 @@ first_descriptions = reviews.description.iloc[0:10]
 sample_reviews = reviews.iloc[[1,2,3,5,8]]
 
 rows = [0,1,10,100]
-cols = ['country', 'province', 'region_1', 'region_2']r
+cols = ['country', 'province', 'region_1', 'region_2']
 df = reviews.loc[rows][cols]
 df2 = reviews.loc[rows, cols]
 df3 = reviews.loc[0:100, 'country':'region_2']
 df4 = reviews.iloc[0:100, 0:4]
-print(df4)
-print(df3)
-print(df2)
-print(df)
+italian_wines = reviews[reviews.country == 'Italy']
+reviews.loc[reviews.country.isin(['Australia','New Zealand'])]
+top_oceania_wines = reviews.loc[reviews.country.isin(['Australia','New Zealand']) & (reviews.points >= 95)]
 
+# Exercises to be done in Lab_1_PD_Wines.py
+# 1.) Identify the list of top wines (wines with score at least 99)
+top_wines = reviews.loc[reviews.points >= 99]
+print(top_wines)
+
+
+	# •	Identify the price of the most expensive wine on the list of top wines
+	# •	Print the details (rows of data) for the most expensive top wine(s)
 
 
